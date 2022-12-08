db93a03e1b89ebc1cc104a5920fbb6b41d738cce
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:25:30 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_2331.java
@@ -0,0 +1,35 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_2331 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String rand0 = scan.nextLine();
+			String var2 = scan.nextLine();
+			int r = sendRequest(conn, rand0 ,var2 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String rand0 ,String var2 ) throws SQLException {
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT EMPLOYEE_ID, START_DATE, END_DATE FROM JOB_HISTORY WHERE START_DATE >" + var2+" OR END_DATE >" + rand0);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
---
+++ DataSet/SySet/before/Dummy_before_2337.java
@@ -0,0 +1,29 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_2337 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String id1 = scan.nextLine();
+			String id2 = scan.nextLine();
+			int r = sendRequest(conn, id1 ,id2 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String id1 ,String id2 ) throws SQLException {
+		String sql = "DELETE FROM DEPARTMENTS WHERE DEPARTMENT_ID IN ("+id1+", "+id2+")";
+		Statement stmt = conn.createStatement();
+		int rs = stmt.executeUpdate(sql);
+		return rs;
+	}
+}
