41311f2e0370d36ee0ced4001e5ebf58673ea697
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 20:28:21 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_1115.java
@@ -0,0 +1,30 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_1115 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String DEPTID = scan.nextLine();
+			String DEPTNAME = scan.nextLine();
+			String LOCATIONID = scan.nextLine();
+			String MANAGERID = scan.nextLine();
+			int r = sendRequest(conn, DEPTID ,DEPTNAME ,LOCATIONID ,MANAGERID );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String DEPTID ,String DEPTNAME ,String LOCATIONID ,String MANAGERID ) throws SQLException {
+		Statement stmt = conn.createStatement();
+		int rs = stmt.executeUpdate("INSERT INTO DEPARTMENTS VALUES ("+DEPTID+","+DEPTNAME+", "+MANAGERID+", "+LOCATIONID+")");
+		return rs;
+	}
+}
---
+++ DataSet/SySet/before/Dummy_before_1181.java
@@ -0,0 +1,35 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_1181 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String var1 = scan.nextLine();
+			int r = sendRequest(conn, var1 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String var1 ) throws SQLException {
+		String sql = "SELECT JOB_ID, MIN_SALARY FROM JOBS WHERE MIN_SALARY <>" + var1;
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
