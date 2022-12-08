198bb86735776123b7fbc02288010298399ccd19
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:26:54 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_2436.java
@@ -0,0 +1,36 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_2436 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String rand1 = scan.nextLine();
+			String var2 = scan.nextLine();
+			int r = sendRequest(conn, rand1 ,var2 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String rand1 ,String var2 ) throws SQLException {
+		String sql = "SELECT EMPLOYEE_ID, END_DATE, JOB_ID FROM JOB_HISTORY WHERE END_DATE >=" + var2+" OR JOB_ID >" + rand1;
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
