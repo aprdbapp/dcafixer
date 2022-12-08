643e8352ccb8cbe44818d623c3e3b6bbc4a54205
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:12:48 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_1801.java
@@ -0,0 +1,35 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_1801 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String rand3 = scan.nextLine();
+			String val1 = scan.nextLine();
+			int r = sendRequest(conn, rand3 ,val1 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String rand3 ,String val1 ) throws SQLException {
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT EMPLOYEE_ID, PHONE_NUMBER, HIRE_DATE FROM EMPLOYEES WHERE PHONE_NUMBER >=" + val1+" AND HIRE_DATE >" + rand3);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
---
+++ DataSet/SySet/before/Dummy_before_1838.java
@@ -0,0 +1,32 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_1838 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String id1 = scan.nextLine();
+			String num = scan.nextLine();
+			int r = sendRequest(conn, id1 ,num );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String id1 ,String num ) throws SQLException {
+		String sql = "INSERT INTO SALES_EMPLOYEES (ID, NAME, SALARY, COMMISSION_PCT) "
+		+"SELECT EMPLOYEE_ID, LAST_NAME, SALARY, COMMISSION_PCT "
+		+"FROM EMPLOYEES "
+		+"WHERE JOB_ID <"+ id1+" AND SALARY > "+num;
+		Statement stmt = conn.createStatement();
+		int rs = stmt.executeUpdate(sql);
+		return rs;
+	}
+}
