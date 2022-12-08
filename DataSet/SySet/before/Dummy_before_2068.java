151ef5dbd57efcf6248e6028fe3075683f94b874
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:20:46 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_2068.java
@@ -0,0 +1,36 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_2068 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String rand6 = scan.nextLine();
+			String val1 = scan.nextLine();
+			int r = sendRequest(conn, rand6 ,val1 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String rand6 ,String val1 ) throws SQLException {
+		String sql = "SELECT EMPLOYEE_ID, SALARY, COMMISSION_PCT FROM EMPLOYEES WHERE SALARY >=" + val1+" OR COMMISSION_PCT >" + rand6;
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
---
+++ DataSet/SySet/before/Dummy_before_2082.java
@@ -0,0 +1,28 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_2082 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String id1 = scan.nextLine();
+			int r = sendRequest(conn, id1 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String id1 ) throws SQLException {
+		String sql = "UPDATE EMPLOYEES SET JOB_ID = 'IT_PROG', COMMISSION_PCT = NULL WHERE EMPLOYEE_ID > "+id1;
+		Statement stmt = conn.createStatement();
+		int rs = stmt.executeUpdate(sql);
+		return rs;
+	}
+}
