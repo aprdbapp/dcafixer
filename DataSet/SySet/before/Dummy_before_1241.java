46d196cc0ae60b161eb5ab793f958939d6239f10
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 20:31:01 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_1241.java
@@ -0,0 +1,35 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_1241 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String rand0 = scan.nextLine();
+			String var1 = scan.nextLine();
+			int r = sendRequest(conn, rand0 ,var1 );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String rand0 ,String var1 ) throws SQLException {
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT COUNTRY_ID, COUNTRY_NAME, REGION_ID FROM COUNTRIES WHERE COUNTRY_NAME =" + var1+" AND REGION_ID =" + rand0);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
