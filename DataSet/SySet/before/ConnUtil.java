5b5ec0d14a2b58ada324e13a809da7ae50a230d0
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:47:19 -0500

    Add files via upload

---
+++ DataSet/SySet/before/ConnUtil.java
@@ -0,0 +1,8 @@
+package datasets.Apps_Before;//package com.dareen;
+public class ConnUtil {	
+	public static  String JDBC_DRIVER = "com.mysql.jdbc.Driver";	
+	public static  String DB_URL = "jdbc:mysql://localhost:3306/DCAFIXER_SCHEMA?serverTimezone=UTC&useSSL=true&allowMultiQueries=true";	
+	public static  String DB_UN = "root";	
+	public static  String DB_PW = "purdue";
+
+}
