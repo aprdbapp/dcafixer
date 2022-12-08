52a0ca99a9f456e7e8b2115ddd3c9161f78ad123
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2022-12-07 22:36:50 -0500

    Add files via upload

---
+++ DataSet/SySet/before/Dummy_before_3077.java
@@ -0,0 +1,38 @@
+package datasets.Apps_Before;
+import java.sql.*;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Dummy_before_3077 {
+	static final String JDBC_DRIVER = ConnUtil.JDBC_DRIVER;
+	static final String DB_URL = ConnUtil.DB_URL;
+	static final String DB_UN = ConnUtil.DB_UN;
+	static final String DB_PW = ConnUtil.DB_PW;
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_UN, DB_PW);
+		try (Scanner scan = new Scanner(System.in)) {
+			String id = scan.nextLine();
+			int r = sendRequest(conn, id );
+			System.out.println(r);
+		}
+	}
+	public static int sendRequest(Connection conn, String id ) throws SQLException {
+		String sql = "SELECT JOB_ID, MAX(SALARY), MIN(SALARY) "
++"FROM EMPLOYEES"
++" GROUP BY JOB_ID"
++"HAVING MAX(SALARY) <" +id;
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		int rows = 0;
+		if (rs != null) {
+			while (rs.next()) {
+				rows++;
+			}
+			rs.close();
+		}
+		return rows;
+	}
+}
