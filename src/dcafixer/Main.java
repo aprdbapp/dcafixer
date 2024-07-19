877bc18fae9cf6199102c0775dbe5efc9cb9003c
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2024-07-19 16:56:36 -0400

    Update Main.java

--- src/dcafixer/Main.java
+++ src/dcafixer/Main.java
@@ -8,14 +8,10 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 
-
 		String projectSrc = "path/to/buggy/project/src/"; // should ends with "/"
 		String projectJar = "path/to/buggy/project/example1.jar";
 		String projectName = "example1";
 
-		
-		
-		
 		Fixer.start_dcafixer(projectName,projectSrc, projectJar);
 	}
 
