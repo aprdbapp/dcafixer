1217f85c40622829f5496e06f09dae70723ef74f
aprdbapp <119972921+aprdbapp@users.noreply.github.com>
2024-07-19 16:25:41 -0400

    Delete src/slicer/datatypes directory

--- src/slicer/datatypes/ClassUsed.java
+++
@@ -1,9 +0,0 @@
-package slicer.datatypes;
-
-public enum ClassUsed {
-	NA, SCAN, // class Scanner
-	CONS, // Class Console
-	BUFREADER, ISREADER, // InputStreamReader
-	FILEREADER // FileReader
-
-}
